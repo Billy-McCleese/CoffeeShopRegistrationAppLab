627c87cf229d71b06d6b1cec2c7145b827a71811
Billy-McCleese <billymccleese@gmail.com>
2023-04-05 22:12:35 -0400

    Submission for coffee shop lab

---
+++ CoffeeShopRegistrationApp/Models/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace CoffeeShopRegistrationApp.Models
+{
+    public class UserProfile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string State { get; set; }
+
+
+    }
+}
+
